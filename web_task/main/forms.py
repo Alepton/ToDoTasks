cd77539ec2c108fc90d61fa3e2d93a85552882ff
Andrei Kardovich <akardovich@gmail.com>
2022-07-07 06:48:35 +0300

    Creating pages for adding tasks to the database part 3

--- web_task/main/forms.py
+++ web_task/main/forms.py
@@ -1,10 +1,11 @@
 from .models import Task
-from django.forms import ModelForm, widgets, TextInput, Textarea
+from django.forms import ModelForm, widgets, TextInput, Textarea, Select
 
 class TaskForm(ModelForm):
     class Meta:
         model = Task
         fields = ["title", "category", "task"]
         widgets = {"title": TextInput(attrs={'class': 'form-control', 'placeholder': 'Введите назавание'}),
+                   "category": Select(attrs={'class': 'select',}, choices=(('Магазин', 'Магазин'), ('Дом', 'Дом'))),
                    "task": Textarea(attrs={'class': 'form-control', 'placeholder': 'Введите описание'}),
                    }
